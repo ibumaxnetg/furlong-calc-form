040213031bd3e9fcb02a39f4ff7970a3eb7a92ba
ibumaxnetg <89496223+ibumaxnetg@users.noreply.github.com>
2021-09-02 13:46:23 +0900

    input Element input element fix

--- src/index.ts
+++ src/index.ts
@@ -21,24 +21,30 @@ const tGH = (event: Event) => {
   const fMaxNum = 8;
   [...Array(fMaxNum)].forEach((ele, index, arr) => {
     const iNum = index + 1;
-    ftimeEl["ftime" + iNum] = Number(
-      document.getElementById(`furlong${iNum}`)!.value
-    );
-    const tmpDisplay = document.querySelector(`.fltime${iNum}`)!;
+    const inputFurlongEl: HTMLInputElement = document.getElementById(
+      `furlong${iNum}`
+    )! as HTMLInputElement;
+    ftimeEl["ftime" + iNum] = Number(inputFurlongEl.value);
+    const tmpDisplay = document.querySelector(
+      `.fltime${iNum}`
+    )! as HTMLSpanElement;
     timeDispEl[iNum] = tmpDisplay;
     // console.log(timeDispEl.index);
-    const praStr = `ftime${iNum}`;
+    // const praStr = `ftime${iNum}`;
   });
   console.log(ftimeEl, timeDispEl);
 
-  timeDispEl[6].textContent = ftimeEl.ftime6 - ftimeEl.ftime5;
-  timeDispEl[5].textContent = ftimeEl.ftime5 - ftimeEl.ftime4;
-  timeDispEl[4].textContent = ftimeEl.ftime4 - ftimeEl.ftime3;
+  timeDispEl[6].textContent = (ftimeEl.ftime6 - ftimeEl.ftime5).toString();
+  timeDispEl[5].textContent = (ftimeEl.ftime5 - ftimeEl.ftime4).toString();
+  timeDispEl[4].textContent = (ftimeEl.ftime4 - ftimeEl.ftime3).toString();
   const tempTime = ftimeEl.ftime3 - ftimeEl.ftime1;
   ftimeEl.ftime2 = tempTime / 2 + ftimeEl.ftime1;
-  document.getElementById("furlong2")!.value = ftimeEl.ftime2;
-  timeDispEl[3].textContent = ftimeEl.ftime3 - ftimeEl.ftime2;
-  timeDispEl[2].textContent = ftimeEl.ftime2 - ftimeEl.ftime1;
+  const furlong2Input: HTMLInputElement = document.getElementById(
+    "furlong2"
+  )! as HTMLInputElement;
+  furlong2Input.value = ftimeEl.ftime2.toString();
+  timeDispEl[3].textContent = (ftimeEl.ftime3 - ftimeEl.ftime2).toString();
+  timeDispEl[2].textContent = (ftimeEl.ftime2 - ftimeEl.ftime1).toString();
   // timeDispEl[iNum].textContent = ftimeEl[praStr];
 };
 
